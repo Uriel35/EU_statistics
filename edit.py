a584896daf52c023ddf015107f670e5a5eba0e4b
Uriel35 <uriespada@gmail.com>
2023-07-21 23:23:13 -0300

    commit 21/07/23

---
+++ edit.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+desercion_22 = pd.read_csv('data/2022/desercion_2022.csv')
+desercion_23 = pd.read_csv('data/2023/desercion_2023.csv')
+# data_23 = pd.read_csv('data/2023/anon_data_2023.csv')
+# data_23 = pd.read_csv('data/2022/anon_data_2022.csv')
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# import re
+import re
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -9,14 +9,14 @@ import numpy as np
 
 
 def vacancies_applicants_chart(desertion_path, data_path, puntaje="Promedio"):
-    def get_promedio_especialidad(data_path, especialidad, resto, puntaje):
-        df = pd.read_csv(data_path)
-        filtro = df['Especialidad'] == especialidad
-        df = df[filtro]
-        df = df.sort_values(by=puntaje, ascending=False)
+    def get_limit(data_path, especialidad, resto, puntaje):
+        data_df = pd.read_csv(data_path)
+        data_df[puntaje] = data_df[puntaje].fillna(0)
+        data_df = data_df[data_df['Especialidad'] == especialidad]
+        data_df = data_df.sort_values(by=puntaje, ascending=False)
         if resto == 1:
-            return df.iloc[-1][puntaje]
-        return df.iloc[-resto - 1][puntaje]
+            return data_df.iloc[-1][puntaje]
+        return data_df.iloc[-resto - 1][puntaje]
 
     df = pd.read_csv(desertion_path)
     df = df.set_index('Especialidad')
@@ -34,8 +34,8 @@ def vacancies_applicants_chart(desertion_path, data_path, puntaje="Promedio"):
             if resto > 0:
                 ax.bar(especialidad_label, [cupos], color=colors.celeste)
                 ax.bar(especialidad_label, [resto], bottom=cupos, color=colors.rojo)
-                promedio = get_promedio_especialidad(especialidad=especialidad, resto=resto, data_path=data_path, puntaje=puntaje)
-                plt.text(especialidad_label, cupos + (max_applicants * 0.06), str(promedio), ha='center', va='top')
+                limit = get_limit(especialidad=especialidad, resto=resto, data_path=data_path, puntaje=puntaje)
+                plt.text(especialidad_label, cupos + (max_applicants * 0.06), str(limit), ha='center', va='top')
             elif resto < 0:
                 ax.bar(especialidad_label, [postulantes], color=colors.celeste)
                 ax.bar(especialidad_label, [resto * (-1)], bottom=postulantes, color=colors.verde)
@@ -124,32 +124,36 @@ def score_variation_by_speciality_chart(data_path, puntaje="Promedio"):
         plt.show()
 
 
-def score_chart(data_path, puntaje='Promedio', interval=0.2):
-    score_df = pd.read_csv(data_path)
-    score_df = score_df.dropna(how='all')
-    score_df = score_df.sort_values(puntaje)
-
-    media = score_df[puntaje].mean()
-
-    conteo = score_df[puntaje].dropna()
-    divisor = 10 / (10 * interval)
-    conteo = conteo.apply(lambda x: round(x * divisor) / divisor)
-
-    conteo = conteo.value_counts().sort_index()
-    if 0 in conteo.index:
-        conteo = conteo.drop(0)
-
+def score_chart(data_paths, years, puntaje='Promedio', interval=0.2):
     fig, ax = plt.subplots()
-    ax.plot(conteo.index, conteo.values, color=colors.celeste)
-    ax.scatter(conteo.index, conteo.values, color=colors.celeste)
+    colores = list(colors.speciality_colors.values())[:len(years)]
+    for year, paths, color in zip(years, data_paths, colores):
+        score_df = pd.read_csv(paths)
+        score_df = score_df.dropna(how='all')
+        score_df = score_df.sort_values(puntaje)
+
+        media = score_df[puntaje].mean()
+
+        conteo = score_df[puntaje].dropna()
+        divisor = 10 / (10 * interval)
+        conteo = conteo.apply(lambda x: round(x * divisor) / divisor)
+
+        conteo = conteo.value_counts().sort_index()
+        if 0 in conteo.index:
+            conteo = conteo.drop(0)
+
+        ax.plot(conteo.index, conteo.values, color=color, label=year)
+        # ax.scatter(media, [50], color='red', label=f'Media {year}')
+
     ax2 = ax.twinx()
     ax2.set_ylim(ax.get_ylim())
-
     ax.set_ylabel('Cantidad')
     ax2.set_ylabel('Cantidad')
     ax.set_xlabel(puntaje)
+    ax.legend()
     plt.suptitle(f'Distribucion de {puntaje} general', fontsize=13)
-    plt.title(f'(Media de {round(media, 2)})', fontsize=9)
+    # plt.title(f'(Media de {round(media, 2)})', fontsize=9)
+
     plt.show()
 
 
@@ -219,11 +223,39 @@ def all_years_score_chart(desertion_paths, interval, puntaje):
     plt.show()
 
 
+def make_reincidence_bar_plot(reincidence_path):
+    reincidence_df = pd.read_csv(reincidence_path)
+
+    especialidad = reincidence_df['Especialidad_vieja_22'].value_counts()
+    nueva_especialidad = reincidence_df['Especialidad_23'].value_counts()
+
+    reincidence_df = pd.concat([especialidad, nueva_especialidad], axis=1, keys=[especialidad.index.name, nueva_especialidad.index.name])
+
+    ## Para que sea mas lindo, acorte hasta el 15... Hay muchas mas pero de poca cantidad
+    reincidence_df = reincidence_df[:15]
+
+    bar_width = 0.35
+    fix, ax = plt.subplots()
+    pos1 = np.arange(len(reincidence_df.index))
+    pos2 = pos1 + bar_width
+    ax.bar(pos1, reincidence_df['Especialidad_vieja_22'], bar_width, label='Especialidad anterior')
+    ax.bar(pos2, reincidence_df['Especialidad_23'], bar_width, label='Especialidad actual')
+    ax.legend()
+    ax.set_xlabel('Especialidad')
+    ax.set_ylabel('Cantidad de personas')
+    ax.set_xticks(pos1 + bar_width / 2)
+    ax.set_xticklabels(reincidence_df.index)
+    ax.tick_params(axis='x', labelrotation=45)
+    plt.title('Reincidencia entre años 2022-2023')
+    plt.show()
+
+
 ### Ejecutar los diferentes plots
 desercion_2022 = 'data/2022/desercion_2022.csv'
 data_2022 = 'data/2022/anon_data_2022.csv'
 desercion_2023 = 'data/2023/desercion_2023.csv'
 data_2023 = 'data/2023/anon_data_2023.csv'
+reincidencia_2023 = 'data/2023/anon_reincidencia_2023.csv'
 
 # all_years_score_chart([data_2022, data_2023], interval=0.2, puntaje='Promedio')
 
@@ -240,13 +272,18 @@ data_2023 = 'data/2023/anon_data_2023.csv'
 # vacancies_pie_chart(desertion_path=desercion_2022)
 
 # vacancies_applicants_chart(desertion_path=desercion_2023, data_path=data_2023, puntaje='Promedio')
+# vacancies_applicants_chart(desertion_path=desercion_2023, data_path=data_2023, puntaje='Puntaje final')
 # desertion_chart(desertion_path=desercion_2023)
 # free_vacancies_chart(desertion_path=desercion_2023)
 # score_variation_by_speciality_chart(data_path=data_2023, puntaje='Examen')
 # score_variation_by_speciality_chart(data_path=data_2023, puntaje='Puntaje final')
 # score_variation_by_speciality_chart(data_path=data_2023, puntaje='Promedio')
-# score_chart(data_path=data_2023, puntaje='Examen', interval=1)
-# score_chart(data_path=data_2023, puntaje='Puntaje final', interval=0.5)
-# score_chart(data_path=data_2023, puntaje='Promedio', interval=0.2)
 # applicants_pie_chart(desertion_path=desercion_2023)
 # vacancies_pie_chart(desertion_path=desercion_2023)
+
+###### Para TODOS los años
+
+make_reincidence_bar_plot(reincidence_path=reincidencia_2023)
+# score_chart(data_paths=[data_2023, data_2022], years=[2023, 2022], puntaje='Examen', interval=1)
+# score_chart(data_paths=[data_2023, data_2022], years=[2023, 2022], puntaje='Puntaje final', interval=1)
+# score_chart(data_paths=[data_2023, data_2022], years=[2023, 2022], puntaje='Promedio', interval=1)
