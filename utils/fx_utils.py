d3e37d6728efe6a05dbe89e43744fdc050efe4e7
Uriel35 <uriespada@gmail.com>
2023-07-30 23:37:15 -0300

    Update Oficial 2023

--- utils/fx_utils.py
+++ utils/fx_utils.py
@@ -1,48 +1,77 @@
+import re
+
+
+def remove_accent(x):
+    dicc = {
+        "á": "a",
+        "é": "e",
+        "í": "i",
+        "ó": "o",
+        "ú": "u",
+        "Á": "A",
+        "É": "E",
+        "Í": "I",
+        "Ó": "O",
+        "Ú": "U",
+    }
+    for key in dicc.keys():
+        x = re.sub(key, dicc[key], x)
+    x = re.sub('(^\s+)|\s+$', '', x)
+    return x
+
 
 def replace_speciality_names(especialidad):
-    if especialidad == "Pediatría y pediátricas articuladas":
-        especialidad = "Pediatría"
-    elif especialidad == "Ortopedia y traumatología":
-        especialidad = 'Traumatología'
-    elif especialidad == "Ortopedia y traumatología infantil":
-        especialidad = 'Traumatología infantil'
-    elif especialidad == "Diagnóstico por imágenes":
-        especialidad = 'Dx por imágenes'
-    elif especialidad == "Cirugía general":
-        especialidad = 'Cirugía'
-    elif especialidad == "Otorrinolaringología":
+    especialidad = remove_accent(especialidad)
+
+    if especialidad == "Pediatria y pediatricas articuladas":
+        especialidad = "Pediatria"
+    elif especialidad == "Ortopedia y traumatologia":
+        especialidad = 'Traumatologia'
+    elif especialidad == "Ortopedia y traumatologia infantil":
+        especialidad = 'Traumatologia infantil'
+    elif especialidad == "Diagnostico por imagenes":
+        especialidad = 'Dx por imagenes'
+    elif especialidad == "Cirugia general":
+        especialidad = 'Cirugia'
+    elif especialidad == "Otorrinolaringologia":
         especialidad = "ORL"
     elif especialidad == "Medicina general y/o medicina de familia":
         especialidad = "Familiar"
-    elif especialidad == "Cirugía infantil (cirugía pediátrica)":
+    elif especialidad == "Cirugia infantil (cirugia pediatrica)":
         especialidad = "Cx infantil"
-    elif especialidad == "Anatomía patológica":
-        especialidad = "Patología"
+    elif especialidad == "Anatomia patologica":
+        especialidad = "Patologia"
     elif especialidad == "Terapia intensiva":
         especialidad = "UTI"
-    elif especialidad == "Cirugía cardiovascular":
+    elif especialidad == "Cirugia cardiovascular":
         especialidad = "Cx cardiovascular"
-    elif especialidad == "Cirugía plástica y reparadora":
-        especialidad = "Cx plástica"
-    elif especialidad == "Fisiatría (medicina física y rehabilitación)":
-        especialidad = "Fisiatría"
+    elif especialidad == "Cirugia plastica y reparadora":
+        especialidad = "Cx plastica"
+    elif especialidad == "Fisiatria (medicina fisica y rehabilitacion)":
+        especialidad = "Fisiatria"
     elif especialidad == "Radioterapia o terapia radiante":
         especialidad = "Radioterapia"
-    elif especialidad == "Cirugía cardiovascular pediátrica":
+    elif especialidad == "Cirugia cardiovascular pediatrica":
         especialidad = "Cx cardiovascular infantil"
-    elif especialidad == "Genética médica":
-        especialidad = 'Genética'
-    elif especialidad == "Alergia e inmunología":
+    elif especialidad == "Genetica medica":
+        especialidad = 'Genetica'
+    elif especialidad == "Alergia e inmunologia":
         especialidad = 'Alergia'
-    elif especialidad == "Medicina Aeronáutica y Espacial":
-        especialidad = 'Aeronáutica y espacial'
-    elif especialidad == "Inmunología . básica":
-        especialidad = 'Inmunología'
-    elif especialidad == "Farmacología clínica":
-        especialidad = 'Farmacología'
-    elif especialidad == "Cirugía de tórax":
-        especialidad = 'Cx de tórax'
-    elif especialidad == "Neurocirugía Pediátrica":
-        especialidad = 'NeuroCx pediátrica'
+    elif especialidad == "Medicina Aeronautica y Espacial":
+        especialidad = 'Aeronautica y espacial'
+    elif especialidad == "Inmunologia . basica":
+        especialidad = 'Inmunologia'
+    elif especialidad == "Farmacologia clinica":
+        especialidad = 'Farmacologia'
+    elif especialidad == "Cirugia de torax":
+        especialidad = 'Cx de torax'
+    elif especialidad == "Neurocirugia Pediatrica":
+        especialidad = 'NeuroCx pediatrica'
+    elif especialidad == 'Neurocirugia':
+        especialidad = 'NeuroCx'
+    elif especialidad == "Ginecologia":
+        especialidad = 'Tocoginecologia'
+    elif especialidad == "Clinica medica":
+        especialidad = 'Clinica'
 
     return especialidad
